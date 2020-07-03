6fe2dec1eb77e5fc30675529415fa5d98099ce5e
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-07-03 12:06:08 +0900

    Order 룰 추가 및 테스트 코드 추가

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ module.exports = {
   "processors": ["stylelint-processor-styled-components"],
   "extends": [
     "stylelint-config-standard",
-    "stylelint-config-styled-components"
+    "stylelint-config-styled-components",
+    "stylelint-config-recess-order",
   ],
   "syntax": "scss",
   "rules": {
-      "selector-type-case": null,
-      "value-keyword-case": null,
-      "comment-empty-line-before": null,
-      "declaration-empty-line-before": null,
-      "selector-type-no-unknown": [true, {
-        "ignore": ["custom-elements", "default-namespace"]
-      }],
-      "no-eol-whitespace": [true, {
-        "ignore": ["empty-lines"]
-      }]
-  }
+    "selector-type-case": null,
+    "value-keyword-case": null,
+    "comment-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "selector-type-no-unknown": [true, {
+      "ignore": ["custom-elements", "default-namespace"],
+    }],
+    "no-eol-whitespace": [true, {
+      "ignore": ["empty-lines"],
+    }],
+  },
 }
